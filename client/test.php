736504a032016dc0239751af87c886c2cd005e5b
sofyanarief <Br!o2018>
2020-05-26 17:02:55 +0700

    repair -m option

--- client/test.php
+++ client/test.php
@@ -1,7 +1,7 @@
 <?php
 // $command=`Client.py -u sopyan`;
 // $command='whoami';
-$command='python3 Client.py -u sopyan -s 192.168.1.21';
+$command='python3 Client.py -u sopyan -s 192.168.1.21 -m reg';
 $return=shell_exec($command);
 echo $return;
 ?>
